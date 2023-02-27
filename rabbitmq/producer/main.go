762a1e3d372c50f5d8c3cedc7e813553ec8f07d6
MdSahil-oss <Mohdssahil1@gmail.com>
2023-02-27 20:19:33 +0530

    Added rabbitmq code
    Signed-off-by: MdSahil-oss <Mohdssahil1@gmail.com>

---
+++ rabbitmq/producer/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://username:password@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		var body string
+
+		fmt.Println("Enter the message:")
+		body, err = bufio.NewReader(os.Stdin).ReadString('\n')
+
+		failOnError(err, "Failed to read message")
+		body = strings.TrimSuffix(body, "\n")
+
+		err = ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s\n", body)
+		cancel()
+	}
+}
